4aeb54bbe4956244e5bffccdffb9a7c51602e33c
Kate Miller <81789816+Kali545@users.noreply.github.com>
2022-06-16 16:08:35 -0700

    My changes
    This is my changes

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -6,12 +6,22 @@ package com.h2;
  */
 public class App 
 {
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
 
     public static int doubleTheNumber(int number) {
-        return -1;
+        return number*2;
     }
+
+    private static int add(int[] numbers){
+        int sum = 0;
+        for(int i = 0; i < numbers.length; i++){
+            sum = sum + numbers[i];
+        }
+        return sum;
+    }
+
 }
